84a04dfc2134c742d5c2602820da3aba200a5001
Paweł Milewczyk <78142724+pawel-milan@users.noreply.github.com>
2021-02-10 09:09:59 +0100

    Adding index.html, style.scss and script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+
+const openModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+};
+
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+};
+
+btnOpenModal.forEach(function(btn){
+    btn.addEventListener('click', openModal)
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key);
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal()
+    }
+});
+
